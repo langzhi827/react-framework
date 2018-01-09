2cfdd56a96a89431e15068b9d6912b1d3acd2eef
harry827 <langzhi@jiankongbao.com>
2018-01-09 23:43:43 +0800

    热更新(HMR)不能和[chunkhash]同时使用

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,16 +5,25 @@
  */
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const webpack = require('webpack');
 
 module.exports = merge(common.baseConfig, {
     devtool: 'eval-source-map ',
     module: {
         rules: [common.cssRules]
     },
+    plugins: [
+        //new webpack.HotModuleReplacementPlugin({})
+    ],
     devServer: {
         contentBase: './dist',
         host: '0.0.0.0',
         compress: true,
-        port: 9999
+        port: 9999,
+        // 需要webpack.HotModuleReplacementPlugin才能完全启用HMR。
+        // 如果使用--hot选项启动webpack或webpack-dev-server，则会自动添加该插件，因
+        // 此您可能不需要将其添加到webpack.config.js中
+        // 注意：热更新(HMR)不能和[chunkhash]同时使用
+        //hot: true
     }
 });
