f8e2ab517129864d801a18ea1da107c9d3ec0f95
harry.lang <harry.lang@yunzhihui.com>
2018-11-29 18:52:30 +0800

    去掉antd的icon映射到本地文件的笨方法

--- src/theme/customize-antd-theme.js
+++ src/theme/customize-antd-theme.js
@@ -3,16 +3,9 @@
  *  Date: 2018/1/12
  *  Description: Created by harrylang on 2018/1/12.
  */
-const path = require('path');
-
-var basePath = process.cwd();
-const iconUrl = path.resolve(basePath, 'src/assets/fonts/antd/font_148784_r2qo40wrmaolayvi');
-// antd icon-url 调用入口
-const antStyleUrl = path.resolve(basePath, 'node_modules/antd/es/style/core');
-
 module.exports = () => {
     return {
         'primary-color': '#1DA57A',
-        'icon-url': '"' + path.relative(antStyleUrl, iconUrl) + '"'
+        'icon-url': '"~assets/fonts/antd/font_148784_r2qo40wrmaolayvi"'
     };
 };
