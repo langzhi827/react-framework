038c1fae9be7c91c9003d147a37bfe6706ee4cb3
harry.lang <harry.lang@yunzhihui.com>
2019-08-27 17:57:30 +0800

    通过webpack提供的ContextReplacementPlugin插件处理momentjs语言包,只打包zh-cn语言包

--- webpack.common.js
+++ webpack.common.js
@@ -122,6 +122,10 @@ let baseConfig = {
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
         }),
+        new webpack.ContextReplacementPlugin(
+            /moment[/\\]locale$/,
+            /zh-cn/
+        ),
         new BundleAnalyzerPlugin({
             analyzerMode: 'server', // static disabled
             analyzerHost: '127.0.0.1',
